fe76cb7d84f7041e98624fa3f4aa2b1c8e3b22df
ketineni27 <ketineni27@gmail.com>
2021-08-17 00:00:23 +0100

    fourth modified commit

--- cicd-pipeline.tf
+++ cicd-pipeline.tf
@@ -102,7 +102,7 @@ resource "aws_codepipeline" "cicd_pipeline" {
             owner = "AWS"
             input_artifacts = ["tf-code"]
             configuration = {
-                ProjectName = "tf-apply"
+                ProjectName = "tf-cicd-apply"
             }
         }
     }
